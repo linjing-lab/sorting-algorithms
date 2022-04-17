f1674f33b1b006a092f4e6ec711dbcfa26f09d42
linjing-lab <1439313331@qq.com>
2022-04-17 17:37:08 +0800

    modified collection/quick_sort.py

--- collection/quick_sort.py
+++ collection/quick_sort.py
@@ -1,6 +1,6 @@
 # lambda实现
 def Lambda(arr):
-	quick_sort = lambda array: array if len(array) <= 1 else quick_sort([item for item in array[1:] if item <= array[0]]) + [array[0]] + quick_sort([item for item in array[1:] if item > array[0]])
+quick_sort = lambda array: array if len(array) <= 1 else quick_sort([item for item in array[1:] if item <= array[0]]) + [array[0]] + quick_sort([item for item in array[1:] if item > array[0]])
 	return quick_sort(arr)
 
 # 递归实现
