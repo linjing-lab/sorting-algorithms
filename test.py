b7953eca7eb29ecc86e50562a14ac1ed8ccd3811
林景 <82669431+linjing-lab@users.noreply.github.com>
2022-08-04 11:35:52 +0800

    test

--- test.py
+++ test.py
@@ -1,17 +1,16 @@
 # 导包
 import time
+from numpy import array
 import polars as pl
 import random
 import copy
-
-from sklearn.decomposition import DictionaryLearning
 
 # 生成测试数据
 data = [random.randint(0, 100) for i in range(10000)]
 
 # 快速排序算法对比
 from collection.quick_sort import Lambda, Recursion, Stack
-def test_Quicksort(array, l=0, r=9999):
+def Quicksort(array, l=0, r=9999):
 	method_list = ["Lambda", "Recursion", "Stack"]
 	dictionary = {}
 	for method in method_list:
@@ -29,11 +28,11 @@ def test_Quicksort(array, l=0, r=9999):
 	    dictionary[method] = gap_list
 	df = pl.DataFrame(dictionary)
 	return df
-print(test_Quicksort(data))
+print(Quicksort(data))
 
 # 归并排序算法的对比
 from collection.merge_sort import Recursion, Stack
-def test_Mergesort(array):
+def Mergesort(array):
 	method_list = ["Recursion", "Stack"]
 	dictionary = {}
 	for method in method_list:
@@ -51,11 +50,11 @@ def test_Mergesort(array):
 	    dictionary[method] = gap_list
 	df = pl.DataFrame(dictionary)
 	return df
-print(test_Mergesort(data))
+print(Mergesort(data))
 
 # 冒泡排序算法的对比
 from collection.bubble_sort import Doubleloop, Flag, Bidirection
-def test_Bubblesort(array):
+def Bubblesort(array):
 	method_list = ["Doubleloop", "Flag", "Bidirection"]
 	dictionary = {}
 	for method in method_list:
@@ -68,4 +67,38 @@ def test_Bubblesort(array):
 		dictionary[method] = gap_list
 	df = pl.DataFrame(dictionary)
 	return df
-print(test_Bubblesort(data))
+print(Bubblesort(data))
+
+# 计数排序算法的对比
+from collection.counting_sort import Whiledivide, Forenumerate, Reversefill
+def Countingsort(array):
+	method_list = ["Whiledivide", "Forenumerate", "Reversefill"]
+	dictionary = {}
+	for method in method_list:
+		function = eval(method)
+		times = time.time()
+		function(copy.deepcopy(array))
+		timee = time.time()
+		gap = round(timee - times, 2)
+		gap_list = [gap]
+		dictionary[method] = gap_list
+	df = pl.DataFrame(dictionary)
+	return df
+print(Countingsort(data))
+
+# 插入排序算法的对比
+from collection.insertion_sort import Direct, Binary
+def Insertionsort(array):
+	method_list = ["Direct", "Binary"]
+	dictionary = {}
+	for method in method_list:
+		function = eval(method)
+		times = time.time()
+		function(copy.deepcopy(array))
+		timee = time.time()
+		gap = round(timee - times, 2)
+		gap_list = [gap]
+		dictionary[method] = gap_list
+	df = pl.DataFrame(dictionary)
+	return df
+print(Insertionsort(data))
