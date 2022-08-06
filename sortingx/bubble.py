2798805757eabdca69104ca2e335c45430f3e19f
林景 <82669431+linjing-lab@users.noreply.github.com>
2022-08-06 10:03:35 +0800

    modified

--- sortingx/bubble.py
+++ sortingx/bubble.py
@@ -9,8 +9,7 @@ def normal(array: List, reverse: bool=False) -> None:
     for i in range(len(array) - 1): # loop to access each array element
         for j in range(len(array) - i - 1): # loop to compare array elements
         # compare two adjacent elements and change > to < to sort in descending order
-            compare = array[j] < array[j + 1] if reverse else array[j] > array[j + 1]
-            if compare:
+            if array[j] < array[j + 1] if reverse else array[j] > array[j + 1]:
                 # swapping elements if elements are not in the intended order
                 array[j], array[j + 1] = array[j + 1], array[j]
 
@@ -23,8 +22,7 @@ def flag(array: List, reverse: bool=False) -> None:
     for i in range(len(array) - 1):
         flag = False # 旗帜
         for j in range(len(array) - i - 1):
-            compare = array[j] < array[j + 1] if reverse else array[j] > array[j + 1]
-            if compare:
+            if array[j] < array[j + 1] if reverse else array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
                 flag = True
         if not flag:
@@ -39,15 +37,13 @@ def bidirect(array: List, reverse: bool=False) -> None:
     for i in range(len(array) - 1):
         flag = False
         for j in range(len(array) - i - 1):
-            compare = array[j] < array[j + 1] if reverse else array[j] > array[j + 1]
-            if compare:
+            if array[j] < array[j + 1] if reverse else array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
                 flag = True
         if flag: # 反向排序
             flag = False
             for j in range(len(array) - i - 2, 0, -1):
-                compare = array[j - 1] < array[j] if reverse else array[j - 1] > array[j]
-                if compare:
+                if array[j - 1] < array[j] if reverse else array[j - 1] > array[j]:
                     array[j], array[j - 1] = array[j - 1], array[j]
                     flag = True
         if not flag:
