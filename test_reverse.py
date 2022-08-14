286b7e9da951dc0f29a25ecd70685e0c87f6748d
林景 <82669431+linjing-lab@users.noreply.github.com>
2022-08-14 18:23:02 +0800

    renew test_reverse

--- test_reverse.py
+++ test_reverse.py
@@ -61,41 +61,61 @@ class Reverser:
         return 'The Design of Reverse of ' + self.method + ' Is Correct!' if self.success == len(self.call) else 'Error!'
 
 # 测试实例1
+'''
 Bubble = Reverser('bubble', ['normal', 'flag', 'bidirect'])
 print(Bubble.collect())
+'''
 
 # 测试实例2
+'''
 Counting = Reverser('counting', ['whilediv', 'forenum', 'reverfill'], ret=[False, False, True])
 print(Counting.collect())
+'''
 
 # 测试实例3
+'''
 Bucket = Reverser('bucket', ['numeric', 'mobase'])
 print(Bucket.collect())
+'''
 
 # 测试实例4
+'''
 Insertion = Reverser('insertion', ['direct', 'binary'])
 print(Insertion.collect())
+'''
 
 # 测试实例5
+'''
 Merge = Reverser('merge', ['recur', 'stack'], ret=[True, False])
 print(Merge.collect())
+'''
 
 # 测试实例6
+'''
 Quick = Reverser('quick', ['lamb', 'recur', 'stack'], part=[False, True, True], ret=[True, False, False])
 print(Quick.collect())
+'''
 
 # 测试实例7
+'''
 Selection = Reverser('selection', ['normal', 'withmax'])
 print(Selection.collect())
+'''
 
 # 测试实例8
+'''
 Shell = Reverser('shell', ['donald', 'knuth', 'hibbard', 'sedgewick'])
 print(Shell.collect())
+'''
 
 # 测试实例9
+'''
 Radix = Reverser('radix', ['lsd', 'msd'], ret=[True, True])
 print(Radix.collect())
+'''
 
 # 测试实例10
+'''
 Heap = Reverser('heap', ['normal', 'recur'])
 print(Heap.collect())
+'''
