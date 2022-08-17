7e64c3dc87be24e18e221f5e951f38063b4ae66f
林景 <82669431+linjing-lab@users.noreply.github.com>
2022-08-17 10:39:26 +0800

    renew utils

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
-def cmp(array1: tuple or list, array2: tuple or list, reverse: bool) -> bool:
-    for index in range(0, len(array1)):
-        if array1[index] > array2[index] and reverse:
+def cmp(left: tuple or list, right: tuple or list, reverse: bool) -> bool:
+    for index in range(0, len(left)):
+        if left[index] > right[index] and reverse:
             return False
-        elif array1[index] > array2[index] and not reverse:
+        elif left[index] > right[index] and not reverse:
             return True
-        elif array1[index] < array2[index] and reverse:
+        elif left[index] < right[index] and reverse:
             return True
-        elif array1[index] < array2[index] and not reverse:
+        elif left[index] < right[index] and not reverse:
             return False
     return False
