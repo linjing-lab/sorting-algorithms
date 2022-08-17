41681b60f7d05d1086743d3818b16f7cc21c2ff7
林景 <82669431+linjing-lab@users.noreply.github.com>
2022-08-17 10:41:51 +0800

    renew utils

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 def cmp(left: tuple or list, right: tuple or list, reverse: bool) -> bool:
+    '''
+    left: 左侧比较数组; right: 右侧比较数组; reverse: 是否逆序
+    '''
     for index in range(0, len(left)):
         if left[index] > right[index] and reverse:
             return False
