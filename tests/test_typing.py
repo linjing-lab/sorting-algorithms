20a8d660bee0653485c040d6601b5a621c59f122
林景 <82669431+linjing-lab@users.noreply.github.com>
2022-11-11 12:27:11 +0800

    renew test

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -1,6 +1,6 @@
 import sortingx as six
 
-data = [('Alex', 100, 90, 98, 95), ('Jack', 97, 88, 98, 92), ('Peter', 92, 95, 92, 96), ('Li', 97, 89, 98, 92)]
-six.bubble(data, key=lambda x: (x[1], x[2]), reverse=False)
+data = (('Alex', 100, 90, 98, 95), ('Jack', 97, 88, 98, 92), ('Peter', 92, 95, 92, 96), ('Li', 97, 89, 98, 92))
+print(six.bubble(data, key=lambda x: (x[1], x[2]), reverse=False))
 # sorted(data, key=lambda x: (x[1], x[2]), reverse=False)
 print(data)
