30dc520ae8d3931cb5cfc96e86303423a33dd10a
林景 <82669431+linjing-lab@users.noreply.github.com>
2023-02-03 19:20:22 +0800

    renew test_.py

--- test_.py
+++ test_.py
@@ -16,6 +16,6 @@ student_objects = (
     Student('Peter', 'B', 10),
 )
 
-test = six.merge(student_objects, key=lambda student: student.name)
-output = sorted(student_objects, key=lambda student: student.name)
+test = six.merge(student_objects, key=attrgetter('grade', 'age'))
+output = sorted(student_objects, key=attrgetter('grade', 'age'))
 print(test, '\n', output)
